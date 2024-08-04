ef90431acda3d0cfdc7d5ef51a4d5fd96bf6dd75
Tonicdev <axeldaniel2004@outlook.com>
2024-08-04 15:59:33 -0700

    Fixed: Added more control to Auth state

--- ChuloBoard/src/contexts/User.jsx
+++ ChuloBoard/src/contexts/User.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
 export const userContext = createContext();
@@ -9,6 +9,8 @@ export function UserProvider({children}) {
         id: "",
         username: ""
     });
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
     const path = useLocation();
     useEffect(() => {
@@ -19,19 +21,43 @@ export function UserProvider({children}) {
                 id: res.data.session.id,
                 username: res.data.session.username
             });
+            setIsAuthenticated(true);
+            setLoading(false);
         }).catch((error) => {
            if(error.response) {
                 setUser({
                     id: "",
                     username: ""
                 });
+                setIsAuthenticated(false);
+                setLoading(false);
                 if(path.pathname.includes("dashboard") || !/\w{0,20}/g.test(path.pathname)) navigate("/login");
            }
         });
         return () => controller.abort();
     }, []);
+
+    const login = useCallback((event) => {
+        event.preventDefault();
+        axios.post(`${import.meta.env.VITE_API_URL}auth`, {username: event.target.username.value, password: event.target.password.value}).then(res => {
+            return navigate("/dashboard/customs");
+        }).catch(error => {
+            return console.error(error);
+        });
+        setLoading(true);
+    }, []);
+
+    const logout = useCallback(() => {
+        axios.delete(`${import.meta.env.VITE_API_URL}auth/logout`).then(res => {
+            navigate("/");
+        }).catch(error => {
+            setError("Hubo un error al cerrar sesión");
+            openErrorAlert();
+        });
+        setLoading(true);
+    }, []);
     return(
-        <userContext.Provider value={user}>
+        <userContext.Provider value={{user, isAuthenticated, logout, login, loading}}>
             {children}
         </userContext.Provider>
     )
--- ChuloBoard/src/hooks/useUser.jsx
+++ ChuloBoard/src/hooks/useUser.jsx
@@ -1,17 +1,10 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { userContext } from "../contexts/User";
-import { useNavigate } from "react-router-dom";
 function useUser() {
     const context = useContext(userContext);
-    const navigate = useNavigate();
-    useEffect(() => {
-        if(!context) {
-            console.error("useUser must be used within a UserProvider");
-        } else if(!context.username || !context.id) {
-           navigate("/login");
-        }
-    }, []);
-
+    if(!context) {
+        console.error("useUser must be used within a UserProvider");
+    }
     return context;
 }
 export {useUser}
