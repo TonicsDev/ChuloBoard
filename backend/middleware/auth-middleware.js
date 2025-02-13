9808317a0a1b1bd7f1fc70a78058f7090efa331c
Tonicdev <axeldaniel2004@outlook.com>
2025-02-13 12:39:06 -0700

    UPDATE: Unauthorized message error changed

--- backend/middleware/auth-middleware.js
+++ backend/middleware/auth-middleware.js
@@ -2,7 +2,7 @@ function isAuth(req, res, next) {
     if(req.isAuthenticated()) {
         next();
     } else {
-        res.status(401).json({error: "UnAuthorized"});
+        res.status(401).json({status: 401, type: "unauthorized", error: "UnAuthorized"});
     }
 }
 
